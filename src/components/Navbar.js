59684036f2af411c2698542ef6dda5292adcfd52
Fazal Haroon <36573782+Fazal-haroon@users.noreply.github.com>
2026-01-14 08:55:26 +0300

    improve the design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
         <nav className={classes.fazal}>
 
             <h2 className={classes.resume}>
-                <a style={{color: 'inherit', textDecoration: 'inherit'}}
+                <a style={{textDecoration: 'inherit'}}
                    href='https://drive.google.com/file/d/1pqBbe7bLNwZ9ZnBYMYN1bHy7FogVth9p/view?usp=sharing'
                    target="_blank" rel="noreferrer">
                     RESUME <FaFileInvoice className={classes.navIcon} />
@@ -24,18 +24,18 @@ const Navbar = () => {
             <ul className={clicked ? classes.mobilelinks : classes.navlinks} onClick={() => setClicked(false)}>
                 <li><NavLink className={({isActive}) =>
                     isActive ? `${classes.about} ${classes.active}` : classes.about
-                } style={{color: 'inherit', textDecoration: 'inherit'}}
+                } style={{textDecoration: 'inherit'}}
                              to="/">ABOUT</NavLink></li>
                 <li><NavLink to="/skills" className={({isActive}) =>
                     isActive ? `${classes.about} ${classes.active}` : classes.about
-                } style={{color: 'inherit', textDecoration: 'inherit'}}>SKILLS</NavLink></li>
+                } style={{textDecoration: 'inherit'}}>SKILLS</NavLink></li>
                 <li><NavLink className={({isActive}) =>
                     isActive ? `${classes.about} ${classes.active}` : classes.about
-                } style={{color: 'inherit', textDecoration: 'inherit'}}
+                } style={{textDecoration: 'inherit'}}
                              to="/project">PROJECTS</NavLink></li>
                 <li><NavLink className={({isActive}) =>
                     isActive ? `${classes.about} ${classes.active}` : classes.about
-                } style={{color: 'inherit', textDecoration: 'inherit'}}
+                } style={{textDecoration: 'inherit'}}
                              to="/experience">EXPERIENCE</NavLink></li>
             </ul>
 
